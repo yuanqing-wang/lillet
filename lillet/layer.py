0fa64fce410afca41ea3e2f555358c9bd5bf15b4
Yuanqing Wang <wangyq+gh@wangyq.net>
2024-05-05 15:06:51 -0400

    radial

--- lillet/layer.py
+++ lillet/layer.py
@@ -1,5 +1,17 @@
 import torch
 
 class LilletLayer(torch.nn.Module):
-    def __init__(self):
-        super(LilletLayer, self).__init__()
+    def __init__(
+            self,
+            mapping: torch.nn.Module,
+    ):
+        super().__init__()
+        self.mapping = mapping
+
+    def forward(
+            self,
+            h: torch.Tensor,
+            x: torch.Tensor,
+    ):
+        h, x = self.mapping(h, x)
+        return h, x
